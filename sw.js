182dac6e3772477d6693cd2e30f8bb1b2be9688d
APX ID <fitra.mbcell@gmail.com>
2026-08-13 18:08:02 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'pmr-spensamo-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  'https://apxid.github.io/pmr/assets/logo.svg'
+];
+
+// Install Service Worker & Cache Resource
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetching Resource (Support Offline)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Update Cache jika ada versi baru
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
